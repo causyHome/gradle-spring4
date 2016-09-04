5d027dd690b3759bd306adc416561935a9c5f392
Florian Causse <causse.florian@gmail.com>
2016-09-04 23:47:55 +0200

    Init project

---
+++ src/main/java/com/causyhome/helloworld/service/HelloWorldService.java
@@ -0,0 +1,28 @@
+package com.causyhome.helloworld.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldService.class);
+
+    public String getDesc() {
+
+        logger.debug("getDesc() is executed!");
+
+        return "Gradle + Spring REST Example";
+
+    }
+
+    public String getTitle(String name) {
+
+        logger.debug("getTitle() is executed!");
+
+        return "Hello World";
+
+    }
+
+}
---
+++ src/main/java/com/causyhome/helloworld/web/WelcomeController.java
@@ -0,0 +1,39 @@
+package com.causyhome.helloworld.web;
+
+import com.causyhome.helloworld.service.HelloWorldService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class WelcomeController {
+
+    private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+    private final HelloWorldService helloWorldService;
+
+    @Autowired
+    public WelcomeController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> index(Map<String, Object> model) {
+
+        logger.debug("index() is executed!");
+
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("title", helloWorldService.getTitle(""));
+        response.put("msg", helloWorldService.getDesc());
+
+        return response;
+    }
+
+}
